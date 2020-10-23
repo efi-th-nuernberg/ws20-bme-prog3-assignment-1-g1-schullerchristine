5dc65ebe2642b7db338dc6d1988c1100d1fd2fb6
repl.it user <replituser@example.com>
2020-10-23 10:00:15 +0000

    update assignment1

--- Main.java
+++ Main.java
@@ -1,5 +1,36 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    int number = 0;
+    String filter = " ";
+    String filter2 = " ";
+    for (int i=0; i<=200; i++) {
+    System.out.println(number = i);
+    filter = divideByFive(number);
+    filter2 = numberEndsWithNine(number);
+    System.out.println(filter + " " +  filter2);
+    }
   }
+  public static String divideByFive(int number) {
+    String result = "";
+    if(number%5==0){
+      result = "Zahl ist durch 5 teilbar";
+    }
+    return(result);
+  }
+
+  public static String numberEndsWithNine(int number) {
+    String result = "";
+    if((number%10)==9){
+      result="Zahl endet auf 9";
+    }
+    return(result);
+  }
+
+  public String twoNumbersDivideByThree(int numberOne, int numberTwo){
+    String result = "Error";
+    if(((numberOne + numberTwo)/3)==0){
+      result ="Zahl und Vorgänger addiert ergeben Ergbenis und Ergbnis ist durch 9 teilbar";
+    }
+    return(result);
+}
 }
